b2e79a8715fd54399b1028801e01285712f3e69b
Sreelekshmi321 <124665790+Sreelekshmi321@users.noreply.github.com>
2024-12-07 01:13:04 +0530

    Update db.py

--- config/db.py
+++ config/db.py
@@ -1,13 +1,6 @@
 # """DATABASE CONNECTION"""
 
 
-# from sqlalchemy import create_engine ,MetaData
-
-# # engine = create_engine("postgresql+psycopg2://postgres:1234@localhost:5432/TASK")
-# # engine = create_engine("postgresql://postgres:1234@localhost:5432/ASSESSMENT")
-# # database details
-# engine = create_engine("postgresql+psycopg2://postgres:1234@localhost:5432/ASSESSMENT")
-
 # meta=MetaData()
 # # onnection to the database
 # conn=engine.connect()
